e8e239d66e722f8dfd52ffdf43d386f14cf0727b
Soham Mulay <woozie.ae@gmail.com>
2026-05-11 21:38:55 +0530

    --force

--- copilot/build_index.py
+++ copilot/build_index.py
@@ -13,9 +13,29 @@ def load_texts():
                             file = f.read()
                             print(file)
                             all_text.append(file)
-    return "\n\n".join(all_text)
+    return "\n\n ".join(all_text)
+
+def split_into_chunks(text):
+    
+    
+    all_chunks =[]
+    total_length = len(text)
+    chunks_size = 400
+    overlap = 50
+    start = 0
+
+    while start < total_length:
+        sliced_text = text[start : start +  chunks_size] 
+        all_chunks.append(sliced_text)
+        gap = chunks_size - overlap 
+        start = start + gap
+    return all_chunks 
+    
+
 load_f = load_texts()
 print(load_f)
+slice_f = split_into_chunks(load_f)
+print(slice_f)
 
     
 
