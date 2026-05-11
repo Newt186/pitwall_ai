5fc85f207bb0e6ebeec1d24fb5a95dbe9cdb6af0
Soham Mulay <woozie.ae@gmail.com>
2026-05-11 20:32:26 +0530

    done with the first step

--- copilot/build_index.py
+++ copilot/build_index.py
@@ -1,16 +1,21 @@
-"""  FUNCTION 1 — load_texts():
-    - Loop through every .txt file in data/raw/
-    - Read the contents of each file
-    - Print the filename as you load it
-    - Join all contents into one big string and return it"""
+
 import os
 
 def load_texts():
-    all_text = ""
+    all_text = []
 
     folder_path = "data/raw"
 
     for file_name in os.listdir(folder_path):
-        
-    for x in 
+                if file_name.endswith(".txt"):
+                        file_path = os.path.join("data/raw/", file_name)
+                        with open(file_path, 'r', encoding = 'utf-8') as f: 
+                            file = f.read()
+                            print(file)
+                            all_text.append(file)
+    return "\n\n".join(all_text)
+load_f = load_texts()
+print(load_f)
+
+    
 
